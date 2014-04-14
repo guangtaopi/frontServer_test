ca3f374681afda97037a2dafcde0c8a0864526ff
gtpi <gtpi@v5.cn>
2014-04-14 10:46:42 +0800

    增加测试类

---
+++ worker/src/test/java/Test.java
@@ -0,0 +1,13 @@
+/**
+ * Created by piguangtao on 14-4-11.
+ */
+public class Test {
+
+    public static  void main(String[] args){
+        int a = 2;
+        long al = a;
+        long g = al << 32;
+        System.out.println("a="+a);
+        System.out.println("g="+g);
+    }
+}
